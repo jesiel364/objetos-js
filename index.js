083a35d70e316a4f5b3714c22f485451435ea9f7
Jesiel364 <82860253+jesiel364@users.noreply.github.com>
2023-06-07 06:01:42 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-import { collection, getDocs } from "firebase/firestore";
 
-const querySnapshot = await getDocs(collection(db, "users"));
-querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${doc.data()}`);
-});
-
-// Write Javascript code!
-const appDiv = document.getElementById('app');
 
 
 const produtos = [];
