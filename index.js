1f99175b0eb8f3356bacc524da09c9054d66f7da
Jesiel364 <82860253+jesiel364@users.noreply.github.com>
2023-06-07 05:50:18 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+import { collection, addDoc } from "firebase/firestore";
 
+try {
+  const docRef = await addDoc(collection(db, "produtos"), {
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+  });
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
