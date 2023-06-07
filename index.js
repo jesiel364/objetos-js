8da1bbcdf5766c6500397178f205582e359d139a
Jesiel364 <82860253+jesiel364@users.noreply.github.com>
2023-06-07 05:00:44 -0400

    update index.js

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ produto2.titulo = 'Feijão';
 produto2.preco = 10.0;
 
 const produto3 = new Object();
-produto1.titulo3 = 'Peixe';
-produto1.preco3 = 15.99;
+produto3.titulo= 'Peixe';
+produto1.preco = 15.99;
 
 produtos.push(produto1, produto2, produto3)
 console.log(produtos);
