31fcdf069bdddf5a1ef3cf4198f1a087c9733411
Jesiel364 <82860253+jesiel364@users.noreply.github.com>
2023-06-07 05:33:52 -0400

    update index.js

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ let tbody = document.getElementById('tbody')
 
 for(let i = 0; i < produtos.length; i++){
 	let tr = tbody.insertRow()
+	
 	appDiv.innerHTML += `<h1>${produtos[i].titulo}</h1>`;
 	appDiv.innerHTML += `<p>${produtos[i].preco}</>`;
 }
