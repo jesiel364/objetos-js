af0894820fee2851bc0d53bb1d6f08a149e98c18
jesiel364 <82860253+jesiel364@users.noreply.github.com>
2023-06-07 04:40:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# objetos-js
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-nn957e)
---
+++ index.js
@@ -0,0 +1,13 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const produtos = [];
+const produto1 = new Object();
+produto.titulo = 'Arroz';
+produto.preco = 4.99;
+
+console.log(produto);
